bb5ed4426bff6389807ecb89ac2296c565b17c74
risheetajain <risheetajain309@gmail.com>
2023-12-29 21:30:34 +0530

    google gemini added

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,221 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_picker/image_picker.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "lib/.env");
+
+  Gemini.init(apiKey: dotenv.env["GOOGLE_API_KEY"].toString());
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Gemini',
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ).copyWith(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+          cardTheme: CardTheme(color: Colors.lightGreenAccent.shade100)),
+      home: const MyGeminiSearchScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyGeminiSearchScreen extends StatefulWidget {
+  const MyGeminiSearchScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyGeminiSearchScreen> createState() => _MyGeminiSearchScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyGeminiSearchScreenState extends State<MyGeminiSearchScreen> {
+  final ImagePicker picker = ImagePicker();
+  final controller = TextEditingController();
+  final gemini = Gemini.instance;
+  String? searchedText, result;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  bool loading = false;
+  bool isTextWithImage = false;
+  Uint8List? selectedImage;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    print("loading $loading");
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Flutter Gemini"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          //Radio to select between different type of search
+          Row(
+            children: [
+              Radio(
+                  value: false,
+                  groupValue: isTextWithImage,
+                  onChanged: (val) {
+                    setState(() {
+                      isTextWithImage = val ?? false;
+                    });
+                  }),
+              const Text("Search with Text"),
+              Radio(
+                  value: true,
+                  groupValue: isTextWithImage,
+                  onChanged: (val) {
+                    setState(() {
+                      isTextWithImage = val ?? false;
+                    });
+                  }),
+              const Text("Search with Text and Image")
+            ],
+          ),
+          //To show text that we have search for
+          if (searchedText != null)
+            MaterialButton(
+                color: Colors.green.shade200,
+                onPressed: () {
+                  setState(() {
+                    searchedText = null;
+                    result = null;
+                  });
+                },
+                child: Text(
+                  'Search: $searchedText',
+                  style: const TextStyle(color: Colors.white),
+                )),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    flex: 2,
+                    //Loader when gemini is searching for result
+                    child: loading
+                        ? const Center(child: CircularProgressIndicator())
+                        : result != null
+                            ? Markdown(
+                                data: result!,
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 12),
+                              )
+                            : const Center(
+                                child: Text('Search something!'),
+                              ),
+                  ),
+                  if (selectedImage != null)
+                    Expanded(
+                      flex: 1,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(32),
+                        child: Image.memory(
+                          selectedImage!,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    )
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Card(
+            margin: const EdgeInsets.all(12),
+            child: Row(
+              children: [
+                Expanded(
+                    child: TextField(
+                  controller: controller,
+                  decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                    hintText: 'Write Something...',
+                    border: InputBorder.none,
+                  ),
+                  onTapOutside: (event) =>
+                      FocusManager.instance.primaryFocus?.unfocus(),
+                )),
+                if (isTextWithImage)
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    //Select image from device
+                    child: IconButton(
+                        onPressed: () async {
+                          final ImagePicker picker =
+                              ImagePicker(); //To select Image from device
+                          // Capture a photo.
+                          final XFile? photo = await picker.pickImage(
+                              source: ImageSource.camera);
+
+                          if (photo != null) {
+                            photo.readAsBytes().then((value) => setState(() {
+                                  selectedImage = value;
+                                }));
+                          }
+                        },
+                        icon: const Icon(Icons.file_copy_outlined)),
+                  ),
+                // Send the typed the search to gemini
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: IconButton(
+                      onPressed: () {
+                        if (controller.text.isNotEmpty) {
+                          if (isTextWithImage) {
+                            if (selectedImage != null) {
+                              searchedText = controller.text;
+                              controller.clear();
+                              loading = true;
+                              setState(() {});
+                              gemini
+                                  .textAndImage(
+                                      text: searchedText!,
+                                      image: selectedImage!)
+                                  .then((value) {
+                                result = value?.content?.parts?.last.text;
+                                loading = false;
+                                   setState(() {});
+                              });
+                            } else {
+                              Fluttertoast.showToast(
+                                  msg: "Please select picture");
+                            }
+                          } else {
+                            searchedText = controller.text;
+                            controller.clear();
+                            loading = true;
+                            setState(() {});
+                            gemini.text(searchedText!).then((value) {
+                              print(value?.content?.parts?.toString());
+                              result = value?.content?.parts?.last.text;
+                              loading = false;
+                                    setState(() {});
+                            });
+                          }
+                        } else {
+                          Fluttertoast.showToast(msg: "Please enter something");
+                        }
+                      },
+                      icon: const Icon(Icons.send_rounded)),
+                ),
+              ],
             ),
-          ],
-        ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
